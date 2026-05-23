c781985e35924430baf054b97e0e471d877861d8
Miguel Ángel Hernández Ortiz <mengelhdz411@gmail.com>
2026-05-22 21:38:48 -0600

    Añadi la opción de seleccionar a los autores dentro del formulario de una nueva obra

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,43 @@ import { useState } from 'react'
 
 function App() {
   const [nuevaObra, setNuevaObra] = useState(false)
-  const [obra, setObra] = useState({titulo: '', clasificacion: ''})
+  const [mostrarAutoresDisponibles, setMostrarAutoresDisponibles] = useState(false)
+  const [obra, setObra] = useState({
+    titulo: '', 
+    clasificacion: '', 
+    autores: []
+  })
   const [obras, setObras] = useState([])
+  const [autoresDisponibles, setAutoresDisponibles] = useState([
+    {id: 1, nombre: 'Autor 1', apellido: 'Apellido 1', email: 'autor1@example.com'},
+    {id: 2, nombre: 'Autor 2', apellido: 'Apellido 2', email: 'autor2@example.com'},
+    {id: 3, nombre: 'Autor 3', apellido: 'Apellido 3', email: 'autor3@example.com'},
+    {id: 4, nombre: 'Autor 4', apellido: 'Apellido 4', email: 'autor4@example.com'},
+  ])
   const [mensaje, setMensaje] = useState('')
 
   const handleChangeObra = (e) => {
     setObra({...obra, [e.target.name]: e.target.value})
   }
 
+  const handleClickAutor = (id) => {
+    // Si el autor ya esta en la lista de autores de la obra entonces lo eliminas, si no esta entonces lo añades
+    obra.autores.includes(id)
+      ? setObra({...obra, autores: obra.autores.filter(autorId => autorId !== id)})
+      : setObra({...obra, autores: [...obra.autores, id]})
+  }
+
   const handleCancelarObra = () => {
     setNuevaObra(false)
-    setObra({titulo: '', clasificacion: ''})
+    setMostrarAutoresDisponibles(false)
+    setObra({titulo: '', clasificacion: '', autores: []})
   }
 
   const handleSubmitObra = (e) => {
     e.preventDefault()
 
     // Validar que los campos no estén vacíos
-    if(!obra.titulo.trim() || !obra.clasificacion) {
+    if(!obra.titulo.trim() || !obra.clasificacion || obra.autores.length === 0) {
       setMensaje('Por favor, completa todos los campos')
       setTimeout(() => {
         setMensaje('')
@@ -27,10 +46,11 @@ function App() {
       return
     }
 
-    // Si la validación es exitosa entonces guardar la obra, escondes el formulario y muestras un mensaje de éxito
+    // Si la validación es exitosa entonces guardas la obra, reseteas el formulario y muestras un mensaje de éxito.
     setObras([...obras, obra])
-    setObra({titulo: '', clasificacion: ''})
+    setObra({titulo: '', clasificacion: '', autores: []})
     setNuevaObra(false)
+    setMostrarAutoresDisponibles(false)
     setMensaje('Obra guardada exitosamente')
     setTimeout(() => {
       setMensaje('')
@@ -58,14 +78,42 @@ function App() {
               <option value="paquete de computo cientifico">Paquete de cómputo científico</option>
               <option value="libro de divulgacion">Libro de divulgación</option>
             </select>
-            <button type="submit">Guardar</button>
-            <button type="button" onClick={handleCancelarObra}>Cancelar</button>
           </label>
+          <button type="button" onClick={() => setMostrarAutoresDisponibles(!mostrarAutoresDisponibles)}>
+            {mostrarAutoresDisponibles ? 'Ocultar Autores' : 'Añadir Autores'}
+          </button>
+          {mostrarAutoresDisponibles && (
+            <div>
+              {autoresDisponibles.map(autor => (
+                <div key={autor.id}>
+                  <p>{autor.nombre} {autor.apellido}</p>
+                  <p>{autor.email}</p>
+                  <button type="button" onClick={() => handleClickAutor(autor.id)}>
+                    {obra.autores.includes(autor.id) ? 'Eliminar Autor' : 'Añadir Autor'}
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+          <button type="submit">Guardar</button>
+          <button type="button" onClick={handleCancelarObra}>Cancelar</button>
         </form>
       )}
       <ul>
         {obras.map((obra, index) => (
-          <li key={index}>{obra.titulo}-{obra.clasificacion}</li>
+          <li key={index}>
+            {obra.titulo}-{obra.clasificacion}
+            {obra.autores.length > 0 && (
+              <ul>
+                {obra.autores.map(autorId => {
+                  const autor = autoresDisponibles.find(a => a.id === autorId)
+                  return (
+                    <li key={autorId}>{autor.nombre} {autor.apellido}</li>
+                  )
+                })}
+              </ul>
+            )}
+          </li>
         ))}
       </ul>
       {mensaje && (
